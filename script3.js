b100076ec0c5d26e635e0f67752a9cdc10f55396
sakia-nisa <sakiatunisa@gmail.com>
2024-02-06 09:43:20 +0700

    modified:   script3.js

--- script3.js
+++ script3.js
@@ -9,6 +9,8 @@ function submitForm() {
 }
 
 const testDiv = document.getElementById("test");
+const deluxeDiv = document.getElementById("deluxe");
+const StandardDiv = document.getElementById("Standard");
 
 function card(nama, harga, gambar, kode, rating) {
     return`
@@ -33,48 +35,22 @@ fetch("https://data-kamar.vercel.app/kamar/suite")
 .then((res) => {
     console.log (res.data);
     res.data.map((kamar) => {
-        testDiv.innerHTML += card(kamar.nama,kamar.harga, kamar.gambar, kamar.kode_kamar, kamar.rating)
+        testDiv.innerHTML += card(kamar.nama,kamar.harga, kamar.gambar, kamar.kode, kamar.rating)
     })
 })
-
-const deluxeDiv = document.getElementById("deluxe");
-
-function card(nama, harga, gambar, kode, rating) {
-    return`
-            <div class="col1">
-                <div class="card1">
-                <div class="row row-cols-1 row-cols-md-2 g-4" id="kartu">
-                    <img src="${gambar}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${nama}</h5>
-                        <p class="card-text">${kode}</p>
-                        <p class="card-text">${rating}</p>
-                        <button type="button" class="btn btn-secondary" data-bs-toggle="offcanvas"
-                            data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">booking</button>
-                        RP. <span class="price">${harga}</span>
-                    </div>
-                </div>
-                </div>
-            </div>`;
-}
-
-const StandardDiv = document.getElementById("Standard");
-
-function card(nama, harga, gambar, kode, rating) {
-    return`
-            <div class="col1">
-                <div class="card1">
-                <div class="row row-cols-1 row-cols-md-2 g-4" id="kartu">
-                    <img src="${gambar}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">${nama}</h5>
-                        <p class="card-text">${kode}</p>
-                        <p class="card-text">${rating}</p>
-                        <button type="button" class="btn btn-secondary" data-bs-toggle="offcanvas"
-                            data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">booking</button>
-                        RP. <span class="price">${harga}</span>
-                    </div>
-                </div>
-                </div>
-            </div>`;
-}
+fetch("https://data-kamar.vercel.app/kamar/deluxe")
+.then((res) => res.json())
+.then((res) => {
+    console.log (res.data);
+    res.data.map((kamar) => {
+        deluxeDiv.innerHTML += card(kamar.nama,kamar.harga, kamar.gambar, kamar.kode, kamar.rating)
+    })
+})
+fetch("https://data-kamar.vercel.app/kamar/standar")
+.then((res) => res.json())
+.then((res) => {
+    console.log (res.data);
+    res.data.map((kamar) => {
+        StandardDiv.innerHTML += card(kamar.nama,kamar.harga, kamar.gambar, kamar.kode, kamar.rating)
+    })
+})
